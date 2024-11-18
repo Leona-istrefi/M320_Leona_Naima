a76cd1cda1aa10bb4f6e8d4cf0d453e95bc5b244
Leona-istrefi <leona.istrefi@edu.tbz.ch>
2024-11-18 15:10:55 +0100

    implemented the setter in the methods

--- coding assignments/D1_TaskB/src/TaskB.java
+++ coding assignments/D1_TaskB/src/TaskB.java
@@ -23,69 +23,38 @@ public class TaskB {
 
     // Method to increase the temperature
     public void increaseTemperature() {
-        if (temperature + increment <= max) {
-            temperature += increment;
+        int newTemperature = temperature + increment;
+        if (newTemperature <= max) {
+            setTemperature(newTemperature);
         } else {
-            temperature = max;
+            setTemperature(max);
         }
         System.out.println("Temperature increased to: " + temperature);
     }
 
     // Method to decrease the temperature
     public void decreaseTemperature() {
-        if (temperature - increment >= min) {
-            temperature -= increment;
+        int newTemperature = temperature - increment;
+        if (newTemperature >= min) {
+            setTemperature(newTemperature);
         } else {
-            temperature = min;
+            setTemperature(min);
         }
         System.out.println("Temperature decreased to: " + temperature);
     }
+
 
     // Getter and setter for temperature
     public int getTemperature() {
         return temperature;
     }
 
+    // Setter for Temparature
     public void setTemperature(int temperature) {
         if (temperature < min || temperature > max) {
             throw new IllegalArgumentException("Temperature must be within the range of min and max");
         }
         this.temperature = temperature;
     }
-
-    // Getter and setter for min
-    public int getMin() {
-        return min;
-    }
-
-    public void setMin(int min) {
-        if (min > max) {
-            throw new IllegalArgumentException("Min cannot be greater than Max");
-        }
-        this.min = min;
-    }
-
-    // Getter and setter for max
-    public int getMax() {
-        return max;
-    }
-
-    public void setMax(int max) {
-        if (max < min) {
-            throw new IllegalArgumentException("Max cannot be less than Min");
-        }
-        this.max = max;
-    }
-
-    // Getter and setter for increment
-    public int getIncrement() {
-        return increment;
-    }
-
-    public void setIncrement(int increment) {
-        if (increment <= 0) {
-            throw new IllegalArgumentException("Increment must be greater than zero");
-        }
-        this.increment = increment;
-    }
+}
 }
